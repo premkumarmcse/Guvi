8495ec272583224ce33b3bc5ebc2c874569fe400
premkumarmcse <premkumar.mcse@ksrce.ac.in>
2016-12-22 06:06:22 -0800

    Add files via upload

---
+++ even.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+
+printf("Enter the number: ");
+scanf("%d",&num);
+if(num%2==0)
+printf("Even");
+else
+printf("odd");
+getch();
+return 0;
+
+
+
+}
