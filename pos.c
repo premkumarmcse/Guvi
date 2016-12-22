93e6908fa9e3f1c7ef643709946ca2c6881737e7
premkumarmcse <premkumar.mcse@ksrce.ac.in>
2016-12-22 02:34:35 -0800

    Add files via upload

---
+++ pos.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+int n;
+printf("Enter the number : ");
+scanf("%d",&n);
+if(n>0)
+printf("Posiive");
+else
+printf("Negative");
+getch();
+return 0;
+
+
+}
