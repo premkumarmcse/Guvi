2b83f05a6a9df7000b39c3910770f28fcd3a633b
premkumarmcse <premkumar.mcse@ksrce.ac.in>
2016-12-28 14:47:43 +0530

    Add files via upload

---
+++ star.c
@@ -0,0 +1,18 @@
+#include"stdio.h"
+#include"conio.h"
+main()
+{
+    int i,j;
+    for(i=1;i<=5;i++)
+    {
+
+        printf("\n");
+
+    for(j=1;j<=i;j++)
+
+    printf("*");
+    }
+return 0;
+getch();
+}
+
