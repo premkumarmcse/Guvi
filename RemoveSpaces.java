7bab66523298d72f24215c1899a398196a588e41
premkumarmcse <premkumar.mcse@ksrce.ac.in>
2017-07-14 15:19:56 +0530

    Add files via upload

---
+++ RemoveSpaces.java
@@ -0,0 +1,17 @@
+package hunter;
+
+import java.util.Scanner;
+
+public class RemoveSpaces {						//level=hunter&set=10
+
+	public static void main(String[] args) {
+
+		Scanner sc=new Scanner(System.in);
+		String s=sc.nextLine();
+		
+		System.out.println(s.trim().replaceAll("a{2,}"," "));
+		sc.close();
+		}
+	}
+
+
